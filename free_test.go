1690b43681e0b065b211167d20cf72c3f9641327
Tobias Schwab <tobias.schwab@dynport.de>
2013-11-12 16:15:31 +0100

    refactor test for "free"

--- free_test.go
+++ free_test.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
+
+var freeResult = map[string]int{
+	"free.mem.total":   8177440,
+	"free.mem.used":    7097488,
+	"free.mem.free":    1079952,
+	"free.mem.buffers": 1230276,
+	"free.mem.cached":  1782336,
+	"free.swap.total":  522236,
+	"free.swap.used":   22176,
+	"free.swap.free":   500060,
+}
 
 func TestFree(t *testing.T) {
 	mh := new(MetricHandler)
 	free := &Free{RawStatus: readFile("fixtures/free.txt")}
 	stats, _ := mh.Collect(free)
 	agg := aggregateStats(stats)
-	assert.Equal(t, agg["free.mem.total"], 8177440)
-	assert.Equal(t, agg["free.mem.used"], 7097488)
-	assert.Equal(t, agg["free.mem.free"], 1079952)
-	assert.Equal(t, agg["free.mem.buffers"], 1230276)
-	assert.Equal(t, agg["free.mem.cached"], 1782336)
-
-	assert.Equal(t, agg["free.swap.total"], 522236)
-	assert.Equal(t, agg["free.swap.used"], 22176)
-	assert.Equal(t, agg["free.swap.free"], 500060)
+	for k, v := range freeResult {
+		assert.Equal(t, agg[k], v, fmt.Sprintf("expected %s to have value %v but was %v", k, v, agg[k]))
+	}
 }
