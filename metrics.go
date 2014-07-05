15cd80a28caf66d363fc1adb749ab352bffb81f3
Tobias Schwab <tobias.schwab@dynport.de>
2014-07-05 09:22:27 +0200

    add type for list of metrics

---
+++ metrics.go
@@ -0,0 +1,15 @@
+package main
+
+type Metrics []*Metric
+
+func (list Metrics) Len() int {
+	return len(list)
+}
+
+func (list Metrics) Swap(a, b int) {
+	list[a], list[b] = list[b], list[a]
+}
+
+func (list Metrics) Less(a, b int) bool {
+	return list[a].Key < list[b].Key
+}
