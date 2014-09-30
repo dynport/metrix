705a8a6a57e8ad65c9f088ae4c7345fda957a5b0
Tobias Schwab <tobias.schwab@dynport.de>
2014-09-30 19:46:29 +0200

    add parsing for load avg

---
+++ metrix/load.go
@@ -0,0 +1,62 @@
+package metrix
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+const (
+	fieldLoad1 = iota
+	fieldLoad5
+	fieldLoad15
+	fieldLoadEntities
+	fieldLoadMostRecentPid
+)
+
+type LoadAVG struct {
+	Min1             float64
+	Min5             float64
+	Min15            float64
+	RunnableEntities int64
+	ExistingEntities int64
+	MostRecentPid    int
+}
+
+func (l *LoadAVG) Load(in io.Reader) error {
+	b, e := ioutil.ReadAll(in)
+	if e != nil {
+		return e
+	}
+	fields := strings.Fields(string(b))
+	if len(fields) != 5 {
+		return fmt.Errorf("expected 5 fields, found %d", len(fields))
+	}
+	l.Min1, e = strconv.ParseFloat(fields[fieldLoad1], 64)
+	if e != nil {
+		return e
+	}
+	l.Min5, e = strconv.ParseFloat(fields[fieldLoad5], 64)
+	if e != nil {
+		return e
+	}
+	l.Min15, e = strconv.ParseFloat(fields[fieldLoad15], 64)
+	if e != nil {
+		return e
+	}
+	parts := strings.Split(fields[fieldLoadEntities], "/")
+	if len(parts) == 2 {
+		l.RunnableEntities, e = parseInt64(parts[0])
+		if e != nil {
+			return e
+		}
+		l.ExistingEntities, e = parseInt64(parts[1])
+		if e != nil {
+			return e
+		}
+	}
+	l.MostRecentPid, e = strconv.Atoi(fields[fieldLoadMostRecentPid])
+	return e
+}
---
+++ metrix/load_test.go
@@ -0,0 +1,26 @@
+package metrix
+
+import (
+	"os"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestLoadAVG(t *testing.T) {
+	Convey("parse", t, func() {
+		f, e := os.Open("fixtures/loadavg.txt")
+		So(e, ShouldBeNil)
+		defer f.Close()
+
+		l := &LoadAVG{}
+		e = l.Load(f)
+		So(e, ShouldBeNil)
+		So(l.Min1, ShouldEqual, 0.00)
+		So(l.Min5, ShouldEqual, 0.01)
+		So(l.Min15, ShouldEqual, 0.05)
+		So(l.RunnableEntities, ShouldEqual, 1)
+		So(l.ExistingEntities, ShouldEqual, 258)
+		So(l.MostRecentPid, ShouldEqual, 8518)
+	})
+}
