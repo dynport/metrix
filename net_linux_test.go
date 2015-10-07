fd7c0380f8465b0d1b4d6dcdb868980ce4aa8efa
Tobias Schwab <tobias.schwab@dynport.de>
2015-10-07 21:45:30 +0200

    comment out tests

--- net_linux_test.go
+++ net_linux_test.go
@@ -9,18 +9,17 @@ import (
 func TestNet(t *testing.T) {
 	SkipConvey("Net", t, func() {
 		t.Skip("not implemented yet")
-		return
+		//return
+		//mh := new(MetricHandler)
+		//net := &Net{}
+		//stats, _ := mh.Collect(net)
+		//So(len(stats), ShouldBeGreaterThan, 4)
+		//So(len(stats), ShouldEqual, 21)
 
-		mh := new(MetricHandler)
-		net := &Net{}
-		stats, _ := mh.Collect(net)
-		So(len(stats), ShouldBeGreaterThan, 4)
-		So(len(stats), ShouldEqual, 21)
+		//So(stats[0].Key, ShouldEqual, "net.ip.TotalPacketsReceived")
+		//So(stats[0].Value, ShouldEqual, int64(162673))
 
-		So(stats[0].Key, ShouldEqual, "net.ip.TotalPacketsReceived")
-		So(stats[0].Value, ShouldEqual, int64(162673))
-
-		So(stats[20].Key, ShouldEqual, "net.ip.OutOctets")
-		So(stats[20].Value, ShouldEqual, int64(667161104))
+		//So(stats[20].Key, ShouldEqual, "net.ip.OutOctets")
+		//So(stats[20].Value, ShouldEqual, int64(667161104))
 	})
 }
